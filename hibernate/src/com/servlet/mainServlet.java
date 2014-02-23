161cfc8a9a0e8c368aa646ddd0ab7760506fee7d
XiaoJin <XiaoJin@XiaoJin-PC.mshome.net>
2014-02-23 23:57:37 +0800

    xiaojin

--- hibernate/src/com/servlet/mainServlet.java
+++ hibernate/src/com/servlet/mainServlet.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +17,7 @@ import com.action.Action;
 
 
 @SuppressWarnings("serial")
-@WebServlet("/")
+
 public class mainServlet extends HttpServlet {
 
 	/**
