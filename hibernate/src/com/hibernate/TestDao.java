152c9684741695cf20c18cf9b1ccdb0058abe965
Administrator <Administrator@192.168.8.128>
2014-02-26 00:03:00 +0800

    TestDao

--- hibernate/src/com/hibernate/TestDao.java
+++ hibernate/src/com/hibernate/TestDao.java
@@ -12,7 +12,7 @@ import org.hibernate.criterion.Restrictions;
 import com.model.myTest;
 import com.util.hibernateUtil;
 
-public class testDao {
+public class TestDao {
 	
 	public boolean Insert(int id,String name,String password) 
 	{
