585c69b82d08ecbceaf4f2b6a70c6a4e6e3ce9d8
Administrator <Administrator@192.168.8.128>
2014-02-25 19:18:48 +0800

    修改getResult到父类中

--- hibernate/src/com/action/user/UserAction.java
+++ hibernate/src/com/action/user/UserAction.java
@@ -1,9 +1,5 @@
 package com.action.user;
 
-import java.lang.reflect.Method;
-
-import javax.servlet.http.HttpServletRequest;
-
 import net.sf.json.JSONObject;
 
 import com.action.Action;
@@ -14,19 +10,6 @@ public class UserAction extends Action{
 	public UserAction() {
 		this.jsonObject = new JSONObject();
 		jsonObject.element("RC", "-2");
-	}
-	public JSONObject getResult(HttpServletRequest request)
-	{
-		this.request = request;
-		String action = request.getParameter("action");
-		try {
-			Method method = this.getClass().getMethod(action);
-			jsonObject = (JSONObject)method.invoke(this);
-			jsonObject.element("RC", 0);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return jsonObject;
 	}
 
 	public JSONObject register() {
