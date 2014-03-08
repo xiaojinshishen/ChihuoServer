06455f529c6208b5da874bd2b51d09b14b8923d5
Administrator <Administrator@192.168.8.128>
2014-03-09 00:52:22 +0800

    LabelInfoAction:addLabel 修改数据库配置:加入数据库驱动

--- hibernate/src/com/action/label/LabelInfoAction.java
+++ hibernate/src/com/action/label/LabelInfoAction.java
@@ -1,7 +1,28 @@
 package com.action.label;
 
 import com.action.Action;
+import com.code.OC;
+import com.code.RC;
+import com.hibernate.LabelInfoDao;
+import com.hibernate.UserInfoDao;
+import com.model.UserInfo;
 
 public class LabelInfoAction extends Action {
-
+	
+	public void addLabel() {
+		String label_name, user_id;
+		try {
+			label_name = request.getParameter("label_name").trim();
+			user_id = request.getParameter("user_id").trim();
+		} catch (Exception e) {
+			jsonObject.put("RC", RC.PARAMETER_ERROR);
+			return;
+		}
+		UserInfo userInfo = new UserInfoDao().getById(user_id);
+		if (userInfo == null) {
+			jsonObject.put("OC", OC.UNKNOWN_USER_ID);
+		} else {
+			jsonObject.put("OC", new LabelInfoDao().Insert(label_name, user_id));
+		}
+	}
 }
