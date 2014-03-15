793c282e1c87315c229963ac2378803c08211179
Administrator <Administrator@192.168.8.128>
2014-03-15 23:41:17 +0800

    DishHistoryAction.java DishInfoAction.java: 添加菜品信息

---
+++ hibernate/src/com/action/dish/DishHistoryAction.java
@@ -0,0 +1,49 @@
+package com.action.dish;
+
+import java.util.List;
+
+import net.sf.json.JSONObject;
+
+import com.action.Action;
+import com.code.OC;
+import com.code.RC;
+import com.model.DishHistory;
+import com.hibernate.DishHistoryDao;
+
+public class DishHistoryAction extends Action {
+
+	public void addDishHistory() {
+		DishHistory dishHistory = new DishHistory();
+		try {
+			dishHistory.setDish_id(Integer.valueOf(request.getParameter("dish_id").trim()));
+			dishHistory.setUser_id(request.getParameter("user_id").trim());
+		} catch (Exception e) {
+			jsonObject.put("RC", RC.PARAMETER_ERROR);
+			return;
+		}
+		jsonObject.put("OC", new DishHistoryDao().Insert(dishHistory));
+	}
+
+	public void getDishHistory() {
+		String user_id = request.getParameter("user_id");
+		int dish_id;
+		try {
+			dish_id = Integer.valueOf(request.getParameter("dish_id"));
+		} catch (Exception e) {
+			dish_id = 0;
+		}
+		if (user_id == null && dish_id == 0) {
+			jsonObject.put("RC", RC.PARAMETER_ERROR);
+			return;
+		} else if (user_id == null && dish_id != 0) {
+			List<?> list = new DishHistoryDao().getByUserId(user_id);
+			if (list == null) {
+				jsonObject.put("OC", OC.FAILIED);
+			} else {
+				jsonObject.put("OC", OC.SUCCESS);
+				jsonObject.put("dish_history_count", list.size());
+				jsonObject.put("dish_histories", JSONObject.fromObject(list));
+			}
+		}
+	}
+}
--- hibernate/src/com/action/dish/DishInfoAction.java
+++ hibernate/src/com/action/dish/DishInfoAction.java
@@ -1,7 +1,25 @@
 package com.action.dish;
 
 import com.action.Action;
+import com.code.RC;
+import com.model.DishInfo;
 
 public class DishInfoAction extends Action {
-
+	
+	public void addDishInfo() {
+		DishInfo dishInfo = new DishInfo();
+		try {
+			dishInfo.setDish_name(request.getParameter("dish_name").trim());
+			dishInfo.setRestaurant_id(Integer.valueOf(request.getParameter("restaurant_id").trim()));
+			dishInfo.setUser_id(request.getParameter("user_id").trim());
+		} catch (Exception e) {
+			jsonObject.put("RC", RC.PARAMETER_ERROR);
+			return;
+		}
+		try {
+			dishInfo.setDish_price(Double.valueOf(request.getParameter("dish_price")));
+		} catch (Exception e) {
+			dishInfo.setDish_price(0);
+		}
+	}
 }
