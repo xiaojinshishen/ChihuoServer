5a3e70321ff25b791c0f30f4c9083013f7cff363
Administrator <Administrator@192.168.8.128>
2014-03-19 22:41:38 +0800

    RestaurantInfoAction.java

--- hibernate/src/com/action/restaurant/RestaurantAction.java
+++
@@ -1,7 +0,0 @@
-package com.action.restaurant;
-
-import com.action.Action;
-
-public class RestaurantAction extends Action {
-
-}
---
+++ hibernate/src/com/action/restaurant/RestaurantInfoAction.java
@@ -0,0 +1,10 @@
+package com.action.restaurant;
+
+import com.action.Action;
+
+public class RestaurantInfoAction extends Action {
+	
+	public void getRestaurantByLocation() {
+		
+	}
+}
